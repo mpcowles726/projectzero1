67f40dbaf85a80da193bd1fbc5c13598ee165b1d
Matthew Cowles <mpcowles726@gmail.com>
2015-10-18 15:10:25 -0700

    inital express server

---
+++ server.js
@@ -0,0 +1,26 @@
+//REQUIREMENTS
+var express = require('express');
+var app = express();
+
+//ROUTES
+app.get('/', function (req, res) {
+	res.send ("WORK");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = app.listen(3000);
+
+
