176705bd8d5eda1f442c322f97ac4a94f6c12bf5
Matthew Cowles <mpcowles726@gmail.com>
2015-10-18 20:13:32 -0700

    added incomplete ajax stuff, just want to move on and come back later

--- public/base.js
+++ public/base.js
@@ -9,17 +9,36 @@ var BlogPost = function(){
 BlogPost.prototype.addEventListeners = function(){
   var that = this;
   console.log("Event listeners added!");
+  
+
+//EVENT HANDLER FOR ADDING A POST
   this.$submitBtn.click(function(e){
 	e.preventDefault();
   alert("form submitted!");
+  $.ajax ({
+    method: "POST",
+    url: "/API/POSTS",
+    footballData: {
+    }
+  })
+    .done(function () {
+      console.log()
+    });
+  });
 	that.addToList(that.$formField.val());
 	that.$formField.val('');
-  });
+  };
+  
+
+// EVENT HANDLER FOR DELETING A POST
   this.$container.click('.glyphicon', function(event){
     that.deleteFromList(event.target.closest('li'));
+    $ajax ({
+      method: "DELETE",
+    }); 
+
 });
-};
-// Adding new posts using prototype function
+ // Adding new posts using prototype function
 BlogPost.prototype.addToList = function(elem){
   var post = new Post(elem);
   this.$container.append(post);
